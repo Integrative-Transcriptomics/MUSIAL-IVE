5b81a8146788726c426cffe3c0222375258cac61
s-t-h <si.ha@live.de>
2022-07-19 10:27:35 +0200

    [UPDATE] The STATE and SETTINGS object from the musial-ive.js script are now added as single .js files.

---
+++ musial-ive-settings.js
@@ -0,0 +1,24 @@
+/**
+    MUSIAL-IVE v2.1
+    Author: Simon Hackl
+    Contact: simon.hackl@uni-tuebingen.de
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/**
+ * The `SETTINGS` variable is used to store variables used by the web-app, e.g. colors.
+ * `SETTINGS` that shall not be changed by user interactions start with a `_` symbol.
+ */
+ export var SETTINGS = {
+    "_verbose": true,
+    "_excludePastFirstTerminationVariants": true
+};
